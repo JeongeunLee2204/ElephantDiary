4a984f63be770f31394415336ffdca9a5800a130
jelee <jelee2204@dgu.ac.kr>
2025-08-06 15:15:02 +0900

    db연결 변경-aws rds 사용

--- src/main/java/com/diary/backend/diary/DiaryController.java
+++ src/main/java/com/diary/backend/diary/DiaryController.java
@@ -6,7 +6,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//elephantdb2 사용-application.properties 참고
 @RestController
 @RequestMapping("/api/diary")
 public class DiaryController {
